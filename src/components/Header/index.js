6b153c850ba6865e5a369276d174e15ca3ace15c
Rafael Damasceno <damscn@gmail.com>
2020-04-09 15:55:09 -0300

    Add Header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { AiOutlineMenuUnfold, AiOutlineMenuFold } from 'react-icons/ai';
+
+import './styles.css';
+
+const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  function handleMenuFold() {
+    setIsMenuOpen(!isMenuOpen);
+  }
+
+  const menuIcon = isMenuOpen ? (
+    <AiOutlineMenuFold size={32} />
+  ) : (
+    <AiOutlineMenuUnfold size={32} />
+  );
+
+  return (
+    <header className="header-container">
+      <div className="content">
+        <h1>Hospital Monitor</h1>
+      </div>
+      <button onClick={handleMenuFold} type="button">
+        {menuIcon}
+      </button>
+    </header>
+  );
+};
+
+export default Header;
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+import Header from './components/Header';
 import Monitor from './pages/Monitor';
 
 const Routes = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Switch>
         <Route path="/" exact component={Monitor} />
       </Switch>
