7f5579e587d2efe3553f4adc44682eae6c7e92f5
Rafael Damasceno <damscn@gmail.com>
2020-04-11 01:07:57 -0300

    Add home button on Header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { AiOutlineMenuUnfold, AiOutlineMenuFold } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
+import { AiOutlineHome, AiOutlineMenuUnfold, AiOutlineMenuFold } from 'react-icons/ai';
 
 import settings from '../../settings';
 
@@ -8,24 +9,25 @@ import './styles.css';
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  function handleMenuFold() {
+  const handleMenuFold = () => {
     setIsMenuOpen(!isMenuOpen);
-  }
+  };
 
-  const menuIcon = isMenuOpen ? (
-    <AiOutlineMenuFold size={32} />
-  ) : (
-    <AiOutlineMenuUnfold size={32} />
-  );
+  const menuIcon = isMenuOpen ? <AiOutlineMenuFold size={32} /> : <AiOutlineMenuUnfold size={32} />;
 
   return (
     <header className="header-container">
       <div className="content">
         <h1>{settings.DASHBOARD_NAME}</h1>
       </div>
-      <button onClick={handleMenuFold} type="button">
-        {menuIcon}
-      </button>
+      <div className="left-button-container">
+        <button className="button" onClick={handleMenuFold} type="button">
+          {menuIcon}
+        </button>
+        <Link className="button-link" to="/">
+          <AiOutlineHome size={32} />
+        </Link>
+      </div>
     </header>
   );
 };
