05d7eb0ccd606116badf19b05e6f77e721cb9b5b
maasouza <maasouza@poli.ufrj.br>
2020-04-12 11:23:51 -0300

    Connect Chart with sensors

--- src/components/Charts/GenericBarChart.js
+++ src/components/Charts/GenericBarChart.js
@@ -2,17 +2,7 @@ import React, { useState, useEffect } from 'react';
 
 import { ResponsiveContainer, BarChart, Bar, XAxis, YAxis, CartesianGrid, Legend, Tooltip } from 'recharts';
 
-const GenericBarChart = ({ data, dataKeyX, dataKeyY, syncId, fillColor, unit, barName }) => {
-
-  const CustomBarLabel = ({ x, y, value, barWidth }) => {
-    return (
-      <text x={x + barWidth / 2} y={y} fill="#777" dy={-6} textAnchor="middle">
-        {value}
-      </text>
-    )
-  }
-
-
+const GenericBarChart = ({ data, dataKeyX, dataKeyY, syncId, fillColor, unit, barName, labelColor }) => {
 
   return (
     <ResponsiveContainer width="100%" height="80%">
@@ -25,8 +15,18 @@ const GenericBarChart = ({ data, dataKeyX, dataKeyY, syncId, fillColor, unit, ba
         <XAxis dataKey={dataKeyX} />
         <YAxis unit={unit} />
         <Tooltip />
-        <Legend layout="horizontal" verticalAlign="top" align="center" />
-        <Bar name={barName} unit={unit} dataKey={dataKeyY} fill={fillColor} label={{ position: 'top' }} />
+        <Legend
+          layout="horizontal"
+          verticalAlign="top"
+          align="center" />
+        <Bar
+          name={barName}
+          unit={unit}
+          dataKey={dataKeyY}
+          fill={fillColor}
+          label={{ formatter: (str) => str + unit, position: 'center', fill: labelColor || 'white', fontSize: '18px', fontWeight: '500' }}
+          isAnimationActive={false}
+        />
       </BarChart>
     </ResponsiveContainer >
   )
--- src/pages/Charts/index.js
+++ src/pages/Charts/index.js
@@ -1,60 +1,41 @@
 import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
 
 import GenericBarChart from '../../components/Charts/GenericBarChart'
+import settings from '../../settings';
 
 import './styles.css';
 
-const Charts = () => {
-
-  const data = [
-    {
-      name: 'Leito 1', uv: 100, pv: 2400, amt: 2400,
-    },
-    {
-      name: 'Leito 2', uv: 123, pv: 1398, amt: 2210,
-    },
-    {
-      name: 'Leito 3', uv: 269, pv: 9800, amt: 2290,
-    },
-    {
-      name: 'Leito 4', uv: 88, pv: 3908, amt: 2000,
-    },
-    {
-      name: 'Leito 5', uv: 76, pv: 4800, amt: 2181,
-    },
-    {
-      name: 'Leito 6', uv: 94, pv: 3800, amt: 2500,
-    },
-    {
-      name: 'Leito 7', uv: 104, pv: 4300, amt: 2100,
-    },
-    {
-      name: 'Leito 8', uv: 69, pv: 4300, amt: 2100,
-    },
-    {
-      name: 'Leito 9', uv: 81, pv: 4300, amt: 2100,
-    },
-    {
-      name: 'Leito 10', uv: 88, pv: 4300, amt: 2100,
-    },
-  ];
+const Charts = ({ data }) => {
 
   return (
     <div className="aggregated-charts-container">
       <div className="info-chart-container">
-        <GenericBarChart data={data} dataKeyX="name" syncId="anyId" dataKeyY="uv" barName="Freq. Cardíaca" unit=" bpm" fillColor="#2fc432" />
+        <GenericBarChart data={data} dataKeyX="name" syncId="anyId" dataKeyY="beat" barName="Freq. Cardíaca" unit=" bpm" fillColor="#2fc432" />
       </div>
       <div className="info-chart-container">
-        <GenericBarChart data={data} dataKeyX="name" syncId="anyId" dataKeyY="pv" barName="SpO2" unit="%" fillColor="#2076e0" />
+        <GenericBarChart data={data} dataKeyX="name" syncId="anyId" dataKeyY="spo2" barName="SpO2" unit="%" fillColor="#2076e0" />
       </div>
       <div className="info-chart-container">
-        <GenericBarChart data={data} dataKeyX="name" syncId="anyId" dataKeyY="amt" barName="Temperatura" unit="°C" fillColor="#e02041" />
+        <GenericBarChart data={data} dataKeyX="name" syncId="anyId" dataKeyY="temp" barName="Temperatura" unit="°C" fillColor="#e02041" />
       </div>
     </div>
   );
 };
 
-export default Charts;
+
+const mapStateToProps = (state, ownProps) => {
+  let data = [];
+  settings.HOSPITAL_BEDS.forEach((hospital_bed) => {
+    const name = hospital_bed.name;
+    const records = state.sensors[hospital_bed.sensor_id];
+    const { beat, spo2, temp } = records[records.length - 1];
+    data.push({ name, beat, spo2, temp })
+  })
+  return { data };
+};
+
+export default connect(mapStateToProps)(Charts);
 
 
 
