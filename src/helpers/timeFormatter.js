74416b4c5bc8a6065592f34cf0dae9d2908cdb21
maasouza <maasouza@poli.ufrj.br>
2020-04-18 21:57:22 -0300

    Add useSeconds parameter

--- src/helpers/timeFormatter.js
+++ src/helpers/timeFormatter.js
@@ -1,10 +1,10 @@
-const timeFormatter = (timestamp) => {
-  var a = new Date(timestamp);
-  var hour = a.getHours();
-  var min = '0' + a.getMinutes();
-  var sec = '0' + a.getSeconds();
-  var time = hour + ':' + min.substr(-2) + ':' + sec.substr(-2);
-  return time;
+const timeFormatter = (timestamp, useSeconds = true) => {
+  const a = new Date(timestamp);
+  const hour = a.getHours();
+  const min = '0' + a.getMinutes();
+  const sec = '0' + a.getSeconds();
+
+  return useSeconds ? hour + ':' + min.substr(-2) + ':' + sec.substr(-2) : hour + ':' + min.substr(-2);
 };
 
 export default timeFormatter;
