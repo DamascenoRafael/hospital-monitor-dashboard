06c4e9171b3bf6bad0cb5170c3f9a762b15f6857
Rafael Damasceno <damscn@gmail.com>
2020-04-19 16:26:26 -0300

    Change temperature display on ReportTable component
    Temperature now always have one decimal place

--- src/components/ReportTable/index.js
+++ src/components/ReportTable/index.js
@@ -37,7 +37,7 @@ const ReportTable = ({ name, reports }) => {
                 </td>
                 <td className="sensor-data-col"> {report.beat} bpm</td>
                 <td className="sensor-data-col"> {report.spo2} %</td>
-                <td className="sensor-data-col"> {report.temp} °C</td>
+                <td className="sensor-data-col"> {isNaN(report.temp) ? '--' : report.temp.toFixed(1)} °C</td>
               </tr>
             ))}
           </tbody>
