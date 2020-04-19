72e97ed7762a1814255c60538ff205ceff86165b
maasouza <maasouza@poli.ufrj.br>
2020-04-18 21:59:28 -0300

    Add Report Page

---
+++ src/pages/Reports/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import Table from '../../components/Table';
+
+import './styles.css';
+
+const Reports = ({ reports, hospitalBeds }) => {
+  return (
+    <div className="monitor-container">
+      <div className="beds-container">
+        {hospitalBeds.map((hospitalBed, id) => (
+          <Table key={id} name={hospitalBed.name} reports={reports[hospitalBed.sensorId].data} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  const reports = state.reports;
+  const hospitalBeds = state.hospitalBeds;
+  return { reports, hospitalBeds };
+};
+
+export default connect(mapStateToProps)(Reports);
