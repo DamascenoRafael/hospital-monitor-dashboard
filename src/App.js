e08d0218e108695a982af6dd044a2b4e7ae09f72
maasouza <maasouza@poli.ufrj.br>
2020-04-21 18:11:26 -0300

    Create handleMessage + Add subscribe on alert topic + Add Toaster component call

--- src/App.js
+++ src/App.js
@@ -4,10 +4,41 @@ import { mapKeys, camelCase } from 'lodash';
 import mqtt from 'mqtt';
 
 import Routes from './routes';
+import AlertToaster from './components/AlertToaster';
 import { hospitalBedsUpdated, sensorDataReceived } from './actions';
+import toaster from 'toasted-notes';
 import settings from 'settings';
 
 import './global.css';
+import 'toasted-notes/src/styles.css';
+
+const handleMessage = (topic, message, sensorDataReceived) => {
+  const [baseTopic, sensorId] = topic.split('/');
+  switch (baseTopic) {
+    case 'alertiot': {
+      const { alertType } = JSON.parse(message.toString());
+      toaster.notify(
+        ({ onClose }) => <AlertToaster hospitalBedId={sensorId} alertType={alertType} onClose={onClose} />,
+        {
+          position: 'bottom-right',
+          duration: null,
+        }
+      );
+      break;
+    }
+    case 'oximetroiot': {
+      const sensorId = topic.split('/').pop();
+      const { beat, spo2, temp } = JSON.parse(message.toString());
+      const timestamp = Date.now();
+      const sensorData = { beat, spo2, temp, timestamp };
+      sensorDataReceived({ sensorId, sensorData });
+      break;
+    }
+    default: {
+      break;
+    }
+  }
+};
 
 const App = ({ hospitalBedsUpdated, sensorDataReceived }) => {
   const hospitalBeds = settings.HOSPITAL_BEDS.map((hospitalBed) =>
@@ -28,15 +59,18 @@ const App = ({ hospitalBedsUpdated, sensorDataReceived }) => {
             console.log(err);
           }
         });
+        client.subscribe(`alertiot/${sensorId}`, function (err) {
+          console.log(`subscribing to alertiot/${sensorId}....`);
+          if (err) {
+            console.log(`error subscribing to alertiot/${sensorId}....`);
+            console.log(err);
+          }
+        });
       });
     });
 
     client.on('message', function (topic, message) {
-      const sensorId = topic.split('/').pop();
-      const { beat, spo2, temp } = JSON.parse(message.toString());
-      const timestamp = Date.now();
-      const sensorData = { beat, spo2, temp, timestamp };
-      sensorDataReceived({ sensorId, sensorData });
+      handleMessage(topic, message, sensorDataReceived);
     });
   });
 
