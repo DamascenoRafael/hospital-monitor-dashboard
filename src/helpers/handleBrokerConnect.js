130fb0a663a5e3edbf9eea2925ce881a1f688443
Rafael Damasceno <damscn@gmail.com>
2020-06-23 14:29:55 -0300

    Minor change on imports

--- src/helpers/handleBrokerConnect.js
+++ src/helpers/handleBrokerConnect.js
@@ -1,10 +1,10 @@
-import settings from 'settings';
+import { OXIMETERS_TOPIC, ALERTS_TOPIC } from 'settings';
 
 const handleBrokerConnect = (client, hospitalBeds) => {
   hospitalBeds.forEach((hospitalBed) => {
     const sensorId = hospitalBed.sensorId;
-    const oximeterTopic = `${settings.OXIMETERS_TOPIC}/${sensorId}`;
-    const alertTopic = `${settings.ALERTS_TOPIC}/${sensorId}`;
+    const oximeterTopic = `${OXIMETERS_TOPIC}/${sensorId}`;
+    const alertTopic = `${ALERTS_TOPIC}/${sensorId}`;
     client.subscribe(oximeterTopic, function (err) {
       console.log(`subscribing to ${oximeterTopic}...`);
       if (err) {
--- src/helpers/handleBrokerMessage.js
+++ src/helpers/handleBrokerMessage.js
@@ -1,17 +1,17 @@
-import settings from 'settings';
+import { OXIMETERS_TOPIC, ALERTS_TOPIC } from 'settings';
 import displayToaster from './displayToaster';
 
 const handleBrokerMessage = (topic, message, sensorDataReceived) => {
   const [baseTopic, sensorId] = topic.split('/');
   const timestamp = Date.now();
   switch (baseTopic) {
-    case settings.ALERTS_TOPIC: {
+    case ALERTS_TOPIC: {
       const { alertType } = JSON.parse(message.toString());
       const alertMessage = `Alerta tipo ${alertType}`;
       displayToaster(sensorId, alertType, alertMessage, timestamp);
       break;
     }
-    case settings.OXIMETERS_TOPIC: {
+    case OXIMETERS_TOPIC: {
       const { beat, spo2, temp } = JSON.parse(message.toString());
       const sensorData = { beat, spo2, temp, timestamp };
       sensorData.temp = temp.toFixed(1);
